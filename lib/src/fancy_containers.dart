95098f82d18a728f97c8bf9ea52e931e1884b809
Aditya Thakur <adityathakurxd@users.noreply.github.com>
2021-07-26 14:13:35 +0530

    Version 0.0.3: Added subtitle, text colors and onTap feature

--- lib/src/fancy_containers.dart
+++ lib/src/fancy_containers.dart
@@ -8,7 +8,9 @@ class FancyContainer extends StatefulWidget {
     this.color1,
     this.color2,
     this.title = "Title",
+    this.textcolor,
     this.subtitle = "Subtitle",
+    this.subtitlecolor,
     this.onTap,
     this.padding,
   }) : super(key: key);
@@ -18,7 +20,10 @@ class FancyContainer extends StatefulWidget {
   final Color? color1;
   final Color? color2;
   final String title;
+  final Color? textcolor;
   final String subtitle;
+  final Color? subtitlecolor;
+  // ignore: prefer_typing_uninitialized_variables
   final Function? onTap;
   final EdgeInsetsGeometry? padding;
 
@@ -29,48 +34,53 @@ class FancyContainer extends StatefulWidget {
 class _FancyContainerState extends State<FancyContainer> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: widget.width??MediaQuery.of(context).size.width * 0.90,
-      height: widget.height,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-        gradient: LinearGradient(
-            colors: [widget.color1??Color(0xFFCB1841), widget.color2??Color(0xFF2604DE)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey,
-            blurRadius: 12,
-            offset: Offset(0, 6),
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Text(
-              '${widget.title}',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: (){widget.onTap;},
+      child: Container(
+        width: widget.width??MediaQuery.of(context).size.width * 0.90,
+        height: widget.height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          gradient: LinearGradient(
+              colors: [widget.color1??Color(0xFFCB1841), widget.color2??Color(0xFF2604DE)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 12,
+              offset: Offset(0, 6),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Text(
+                '${widget.title}',
+                style: TextStyle(
+                  color: widget.textcolor,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Text(
-             widget.subtitle,
-              style: TextStyle(
-                fontSize: 15.0,
-                // fontFamily: 'Montserrat',
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Text(
+               widget.subtitle,
+                style: TextStyle(
+                  color: widget.subtitlecolor,
+                  fontSize: 15.0,
+                  // fontFamily: 'Montserrat',
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
